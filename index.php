27f88ae09921fcbdcf2a8207628d70355e055096
Lars Eschweiler <lars.eschweiler@novacapta.com>
2025-12-23 15:00:54 +0100

    fix: compact view

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ function egj_render_small_calendar_events($arrayOfEvents)
     }   
 
     $renderedAppointment = egj_load_and_render_template('template_appointment_small.html', array(
-      'linkText' => esc_html($summary),
+      'linkText' => $summary,
       'dateTimeInfo' => $renderedDateTimeInfo
     ));
 
